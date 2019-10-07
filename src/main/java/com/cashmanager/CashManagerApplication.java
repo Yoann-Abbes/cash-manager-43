1a304e23ce78ef264aa4e5f1aa97b7ea4cbd6586
touria oudadsse <toudadsse@gmail.com>
2019-10-07 14:15:46 +0200

    Projet
    Projet cash manager 1ere itération

---
+++ src/main/java/com/cashmanager/CashManagerApplication.java
@@ -0,0 +1,14 @@
+package com.cashmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashManagerApplication.class, args);
+	}
+
+	
+}
