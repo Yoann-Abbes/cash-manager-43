619830bdafcdcaba04f2d0326eddd227a9755f73
Yoann <yoann1.abbes@epitech.eu>
2019-10-22 17:24:15 +0200

    fix script

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 main() {
-    mix ecto.reset
     docker-compose up -d
     npm install -g newman
     newman run ./tests/Tests-collection.json --timeout 18000000 --timeout-request 18000000 --timeout-script 18000000 --bail
