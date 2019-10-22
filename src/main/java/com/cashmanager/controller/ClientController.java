0cebbfbeba405106a60d049677cdf902cfd7be37
Yoann-Abbes <44844010+Yoann-Abbes@users.noreply.github.com>
2019-10-22 16:00:31 +0200

    add Client Controller (#5)
    This PR Adds ClientController with CRUD Routes
    
    GET /clients (returns all clients)
    GET /clients/{id} (returns one client)
    POST /clients (create)
    PUT /clients/{id} (update)
    DELETE /clients/{id} (delete)
    
    NOTE: I noticed that SpringBoot converts camel case to snake_case:
    To avoid confusion, please use snake_case everywhere
    
    OTHER CHANGES
    The Database conception changed, i removed useless references.
    Now Panier references client and produit
    And paiement references mode_paiement and panier
    
    Be careful, everything is in snake case, meaning you have to use underscores, and not use uppercase letters.

---
+++ src/main/java/com/cashmanager/controller/ClientController.java
@@ -0,0 +1,64 @@
+package com.cashmanager.controller;
+
+import com.cashmanager.model.Client;
+import com.cashmanager.repository.ClientRepository;
+import com.cashmanager.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    // Get All Clients
+    @GetMapping(value = "/clients")
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    // Create a new Clients
+    @PostMapping("/clients")
+    public Client createClient(@Valid @RequestBody Client client) {
+        return clientRepository.save(client);
+    }
+
+    // Get a Single Client by id
+    @GetMapping(value = "/clients/{idClient}")
+    public Client getClientById(@PathVariable Long idClient) {
+        return clientRepository.findById(idClient)
+                .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
+    }
+
+
+    // Update a Client
+    @PutMapping(value = "/clients/{idClient}")
+    public Client updateClient(@PathVariable Long idClient,
+                             @Valid @RequestBody Client clientDetails) {
+
+        Client client = clientRepository.findById(idClient)
+                .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
+
+        client.setNom(clientDetails.getNom());
+        client.setPrenom(clientDetails.getPrenom());
+
+        Client updatedClient = clientRepository.save(client);
+        return updatedClient;
+    }
+
+    // Delete a Client
+    @DeleteMapping(value = "/clients/{idClient}")
+    public ResponseEntity<?> deleteClient(@PathVariable Long idClient) {
+        Client client = clientRepository.findById(idClient)
+                .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
+
+        clientRepository.delete(client);
+
+        return ResponseEntity.ok().build();
+    }}
+
+
