bb6cfd304bddcab9e9f7bd1920b6b5572035ca3b
Yoann <yoann1.abbes@epitech.eu>
2019-10-22 17:43:19 +0200

    update script

--- tests/test.sh
+++ tests/test.sh
@@ -2,6 +2,11 @@
 
 main() {
     docker-compose up -d
+
+    until curl --output /dev/null --silent --fail -d -H "Content-Type: application/json" -X GET http://localhost:8080/clients; do
+ printf 'Waiting ...'
+ sleep 5
+done
     npm install -g newman
     newman run ./tests/Tests-collection.json --timeout 18000000 --timeout-request 18000000 --timeout-script 18000000 --bail
     status=$?
