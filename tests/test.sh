9140724cfddc622e4320c6a67da17411182d4873
Yoann <yoann1.abbes@epitech.eu>
2019-10-22 17:18:32 +0200

    script for unit api test, update travis yml

---
+++ tests/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+main() {
+    mix ecto.reset
+    docker-compose up -d
+    npm install -g newman
+    newman run ./tests/Tests-collection.json --timeout 18000000 --timeout-request 18000000 --timeout-script 18000000 --bail
+    status=$?
+    exit $status
+}
+
+main
+
