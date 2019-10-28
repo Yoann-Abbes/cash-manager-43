0278ffbafc9349292fd98d287d4661a926dc4bec
Yoann <yoann1.abbes@epitech.eu>
2019-10-28 11:33:33 +0100

    doc swagger Client Controller

--- src/main/java/com/cashmanager/controller/ClientController.java
+++ src/main/java/com/cashmanager/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.cashmanager.controller;
 import com.cashmanager.model.Client;
 import com.cashmanager.repository.ClientRepository;
 import com.cashmanager.exception.ResourceNotFoundException;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,21 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/clients/")
+@Api(value = "Client Controller")
 public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
 
     // Get All Clients
+    @ApiOperation(value = "View a list of available employees", response = List.class)
+    @ApiResponses(value = {
+    @ApiResponse(code = 200, message = "Successfully retrieved list"),
+    @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(value = "/clients")
     public List<Client> getAllClients() {
         return clientRepository.findAll();
@@ -23,21 +33,24 @@ public class ClientController {
 
     // Create a new Clients
     @PostMapping("/clients")
+    @ApiOperation(value = "Add an client")
     public Client createClient(@Valid @RequestBody Client client) {
         return clientRepository.save(client);
     }
 
     // Get a Single Client by id
+    @ApiOperation(value = "Get an Client by Id")
     @GetMapping(value = "/clients/{idClient}")
-    public Client getClientById(@PathVariable Long idClient) {
+    public Client getClientById(@ApiParam(value = "Client id from which client object will retrieve", required = true) @PathVariable Long idClient) {
         return clientRepository.findById(idClient)
                 .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
     }
 
 
     // Update a Client
+    @ApiOperation(value = "Update a client")
     @PutMapping(value = "/clients/{idClient}")
-    public Client updateClient(@PathVariable Long idClient,
+    public Client updateClient(@ApiParam(value = "Employee Id to update employee object", required = true) @PathVariable Long idClient,
                              @Valid @RequestBody Client clientDetails) {
 
         Client client = clientRepository.findById(idClient)
@@ -52,7 +65,7 @@ public class ClientController {
 
     // Delete a Client
     @DeleteMapping(value = "/clients/{idClient}")
-    public ResponseEntity<?> deleteClient(@PathVariable Long idClient) {
+    public ResponseEntity<?> deleteClient(@ApiParam(value = "Client Id from which client object will delete from database table", required = true) @PathVariable Long idClient) {
         Client client = clientRepository.findById(idClient)
                 .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
 
--- src/main/java/com/cashmanager/model/Client.java
+++ src/main/java/com/cashmanager/model/Client.java
@@ -1,6 +1,9 @@
 package com.cashmanager.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.beans.JavaBean;
@@ -9,17 +12,20 @@ import java.io.Serializable;
 @Entity
 @Table(name = "Client")
 @JavaBean
-//@JsonIgnoreProperties(allowGetters = true)
+@ApiModel(description = "All details about the Client. ")
 
 public class Client implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(notes = "The database generated client ID")
     private Long idClient;
 
     @Column(name = "nom")
+    @ApiModelProperty(notes = "The client last name")
     private String nom;
 
     @Column(name = "prenom")
+    @ApiModelProperty(notes = "The client first name")
     private String prenom;
 
     public Long getId() {
