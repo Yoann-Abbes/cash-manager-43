c24fae11c851532a04ca0aecef6da8853b8caae0
alexis44119 <alexis.breheret@gmail.com>
2019-11-12 15:30:57 +0100

    reversed changes to not include abdel's changes

--- src/main/java/com/cashmanager/controller/ClientController.java
+++ src/main/java/com/cashmanager/controller/ClientController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.persistence.ElementCollection;
 import javax.validation.Valid;
 import java.util.List;
@@ -22,10 +23,10 @@ public class ClientController {
     // Get All Clients
     @ApiOperation(value = "View a list of available employees", response = List.class)
     @ApiResponses(value = {
-    @ApiResponse(code = 200, message = "Successfully retrieved list"),
-    @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-    @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-    @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
     })
     @GetMapping(value = "/clients")
     public List<Client> getAllClients() {
@@ -52,7 +53,7 @@ public class ClientController {
     @ApiOperation(value = "Update a client")
     @PutMapping(value = "/clients/{idClient}")
     public Client updateClient(@ApiParam(value = "Employee Id to update employee object", required = true) @PathVariable Long idClient,
-                             @Valid @RequestBody Client clientDetails) {
+                               @Valid @RequestBody Client clientDetails) {
 
         Client client = clientRepository.findById(idClient)
                 .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
@@ -66,9 +67,7 @@ public class ClientController {
 
     // Delete a Client
     @DeleteMapping(value = "/clients/{idClient}")
-    public ResponseEntity<?> deleteClient(
-            @ApiParam(value = "Client Id from which client object will delete from database table", required = true)
-                                              @PathVariable Long idClient) {
+    public ResponseEntity<?> deleteClient(@ApiParam(value = "Client Id from which client object will delete from database table", required = true) @PathVariable Long idClient) {
         Client client = clientRepository.findById(idClient)
                 .orElseThrow(() -> new ResourceNotFoundException("Client", "idClient", idClient));
 
@@ -76,5 +75,4 @@ public class ClientController {
 
         return ResponseEntity.ok().build();
     }}
-
 
