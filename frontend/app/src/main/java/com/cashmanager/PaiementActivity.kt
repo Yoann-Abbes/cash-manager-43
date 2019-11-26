3095a0b32913d59a8ee35ee591fa9bcccb9c8d7b
touria oudadsse <toudadsse@gmail.com>
2019-11-26 11:59:16 +0100

    Add paiement
    Add  different mode paiement

--- frontend/app/src/main/java/com/cashmanager/PaiementActivity.kt
+++ frontend/app/src/main/java/com/cashmanager/PaiementActivity.kt
@@ -1,7 +1,9 @@
 package com.cashmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_paiement.*
 
 class PaiementActivity : AppCompatActivity() {
 
@@ -9,6 +11,13 @@ class PaiementActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paiement)
 
-
+        button6.setOnClickListener {
+            val intent = Intent(this, PaiementNfc ::class.java)
+            startActivity(intent)
     }
+        button7.setOnClickListener {
+            val intent = Intent(this, PaiementchequeActivity ::class.java)
+            startActivity(intent)
+        }
+}
 }
