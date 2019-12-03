105337966043fa34026c4042ccb503d4365e8dd1
alexis44119 <alexis.breheret@gmail.com>
2019-12-03 22:03:04 +0100

    starting login by id

--- frontend/app/src/main/java/com/cashmanager/ServerConnection.kt
+++ frontend/app/src/main/java/com/cashmanager/ServerConnection.kt
@@ -1,16 +1,20 @@
 package com.cashmanager
 
+import androidx.appcompat.app.AppCompatActivity
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import java.io.IOException
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_panier.*
 
-class AsynchronousGet {
+class AsynchronousGet: AppCompatActivity() {
     private val client = OkHttpClient()
 
     fun run(id: String?) {
+        var resp: String
         val request = Request.Builder()
             .url("http://18.191.247.228:8080/clients/"+id)
             .build()
@@ -22,14 +26,20 @@ class AsynchronousGet {
 
             override fun onResponse(call: Call, response: Response) {
                 response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    //if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-                    for ((name, value) in response.headers) {
+                   /* for ((name, value) in response.headers) {
                         println("$name: $value")
-                    }
-                    if (response.code != 200){
+                    }*/
+
+                    if (response.code == 200){
+                        resp = "ok"
+                        println(response.code)
                         println(response.body!!.string() + "id : " + id)
+                        // var intent = Intent(this, PanierActivity::class.java)
                 } else {
+                        resp = "ok"
+                        println(response.code)
                         println("echec")
                     }
                 }
